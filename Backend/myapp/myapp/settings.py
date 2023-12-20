7d6d6f01fa3a0c3cdc2588ede3f4c44b47d48746
valmir <valmirchagas88@gmail.com>
2023-12-20 09:45:00 -0300

    chore: add new requirements items

--- Backend/myapp/myapp/settings.py
+++ Backend/myapp/myapp/settings.py
@@ -48,6 +48,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "myapp.urls"
