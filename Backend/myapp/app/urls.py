6a48c233bec439945f6a846caa63df76ceec6edf
valmir <valmirchagas88@gmail.com>
2023-12-19 22:04:28 -0300

    feat: add create task feature

--- Backend/myapp/app/urls.py
+++ Backend/myapp/app/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     # path("create-task/", views.app, name="app"),
     path("create-task/", views.create_task, name="app"),
+    # path("read-task/", views.read_tasks, name="app"),
 
 ]
--- Backend/myapp/app/views.py
+++ Backend/myapp/app/views.py
@@ -1,12 +1,3 @@
-# from django.http import HttpResponse
-
-
-# def app(request):
-#     response = HttpResponse("Hello world>>>>>>>")
-
-#     return response
-
-
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Task
@@ -31,3 +22,19 @@ def create_task(request):
 
     else:
         return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+
+# def read_tasks(request):
+#     if request.method == 'GET':
+#         tasks = Task.objects.all()
+
+#         task_list = [{
+#             "id": task.id,
+#             "task": task.task,
+#             "status": task.status}
+#             for task in tasks]
+
+#         return JsonResponse({'tasks': task_list})
+
+#     else:
+#         return JsonResponse({'error': 'Método não permitido'}, status=405)
